0ac741dd339219124aa74c27fccab77db20b52f3
Goinmul <21400045@handong.edu>
2018-10-30 15:11:07 +0900

    MovieData is analyzed before, but forgot to commit, so committing now.

--- src/main/java/edu/handong/csee/pp1/bigdata/movielens/MovieData.java
+++ src/main/java/edu/handong/csee/pp1/bigdata/movielens/MovieData.java
@@ -8,14 +8,26 @@ import org.apache.commons.configuration.* ;
 
 public class MovieData 
 {
+	//1. HashSet : basically hashmap(instance from hashmap). Fast, since it doesn't provide
+	// 			   sorting. It only contains unique elements(no redundancy).
+	
+	// ex) {1, 2, 5, 3, 6, 9, 11}
+	
+	//2. TreeMap : basically a hashmap, but it is arranged in a natural order.
+	//			   Hashmap allows one null key, but treemap doesn't take a null key.
+	//			   Provide speed, less memory need, but the tree balance should be balanced(hard to implement)
+	
+	// ex) {720(user_id), {1, 2, 5}}
+	//	   {721, {4, 1, 9}}
+	//	   ...
 	TreeMap<Integer, HashSet<Integer>>
-	Baskets = new TreeMap<Integer, HashSet<Integer>>() ;
+	Baskets = new TreeMap<Integer, HashSet<Integer>>() ;	// Baskets = { USER_ID, {movie1, movie2, ...} }
 
 	TreeMap<Integer, Integer>
-	numRatingsOfMovies = new TreeMap<Integer, Integer>() ;
+	numRatingsOfMovies = new TreeMap<Integer, Integer>() ;	// { MOVIE_NUMBER, 4.0 }
 
 	TreeMap<Integer, Double>
-	accumulatedRatingsOfMovies = new TreeMap<Integer, Double>() ;
+	accumulatedRatingsOfMovies = new TreeMap<Integer, Double>() ;	// { MOVIE_NUMBER, 3.75 }
 
 	PropertiesConfiguration config ;
 	double like_threshold ;
@@ -35,26 +47,33 @@ public class MovieData
 			Integer movie  = Integer.parseInt(r.get(1)) ;
 			Double  rating = Double.parseDouble(r.get(2)) ;
 
-			// count ratings and accumulate rating scores by a user
-			if (numRatingsOfMovies.containsKey(movie) == false) {
-				numRatingsOfMovies.put(movie, 1) ; // first rating count
+			// 1) count number of ratings 
+			// 2) accumulate rating scores by a user
+			if (numRatingsOfMovies.containsKey(movie) == false) {	// the very first introduction of a movie, it doesn't contain a key. So it is false.
+				numRatingsOfMovies.put(movie, 1) ;
 				accumulatedRatingsOfMovies.put(movie, rating) ;
 			}
 			else {
-				numRatingsOfMovies.put(movie, numRatingsOfMovies.get(movie) + 1) ;
+				numRatingsOfMovies.put(movie, numRatingsOfMovies.get(movie) + 1) ;	// if the movie had been put before: add up the number of ratings(how many)
 				accumulatedRatingsOfMovies.put(movie, accumulatedRatingsOfMovies.get(movie) + rating) ;
 			}
 
 			// Deal with a good rating based on the like_threshold.
-			// We consider that a user likes the movie when he/she rates the movie not less than this score.
+			// We consider that a user likes the movie when he/she rates the movie more than this score.
 			// If a user likes this movie, put it in the Basket for getting association rules
 			if (rating >= like_threshold) {
+				// Get(take, I'd say.) the basket of this user.
+				HashSet<Integer> basket = Baskets.get(user) ; // this means that pick up the value according to the key.
+															  // At first, of course, Baskets.get(user) returns null.
+															  // in Baskets = { 720(key), {harry_porter_1}(value) }
 				
-				// Get the basket of this user.
-				HashSet<Integer> basket = Baskets.get(user) ;
-				if (basket == null) { // no Basket for the user? Create one.
+				// basket = { harry_porter_1, bourne_legacy, ... } it contains movie numbers(as integer form), and these movies' ratings are more than 4.0(threshold).
+				// Baskets = { 720, {harry_porter_1} } it contains one user_id and one movie set.
+				
+				if (basket == null) { // no Basket for the user? Create one(form of Baskets).
+									  // data looks like : 170, 47, 5.0
 					basket = new HashSet<Integer>() ;
-					Baskets.put(user, basket) ;
+					Baskets.put(user, basket) ;	// put a new element of TreeMap, something like { 840 , { harry_porter_1, bourne_identity, ... } }
 				}
 				
 				// put the movie for the user.
@@ -65,10 +84,10 @@ public class MovieData
 
 	public void removeOutliers() {
 		HashSet<Integer> outliers = new HashSet<Integer>() ;
-		for (Integer userId : Baskets.keySet()) {
+		for (Integer userId : Baskets.keySet()) {	// literally, set of keys(user_id). {720, 721, 722, ...}
 			HashSet<Integer> basket = Baskets.get(userId) ;
-			if (basket.size() > outlier_threshold) 
-				outliers.add(userId) ;
+			if (basket.size() > outlier_threshold) // if basket size is bigger than 100(outlier_threshold)
+				outliers.add(userId) ;	// make a blacklist to delete them(at below code)
 		}
 		System.out.print("Outlier removed: ");
 		int i=0;
